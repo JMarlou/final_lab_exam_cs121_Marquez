3605c54015b7296b838933ddfc800deef711de99
JMarlou <165748727+JMarlou@users.noreply.github.com>
2024-05-14 03:10:47 +0000

    first

---
+++ utils/user_manager.py
@@ -0,0 +1,15 @@
+class UserManager:
+    def load_user():
+        pass
+
+    def save_users():
+        pass
+
+    def validate_username():
+        pass
+
+    def register():
+        pass
+
+    def login():
+        pass
