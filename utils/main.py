76e61f16646290b645ff16dc6fcc9de92f5050a7
JMarlou <165748727+JMarlou@users.noreply.github.com>
2024-05-14 04:05:11 +0000

    save1

--- utils/main.py
+++ utils/main.py
@@ -1,7 +1,18 @@
-import user_manager
+from user_manager import *
+Register = UserManager
 
 def main():
-	print("Hello")
+    print("Welcome to Dice Roll Game!")
+    print("1. Register")
+	print("2. Login")
+	print("3. Exit")
+	
+    choice = input("Action: ")
+
+    if choice == '1':
+        Register.register()
+    else:
+        print("No")
 
 
 if __name__ == "__main__":
